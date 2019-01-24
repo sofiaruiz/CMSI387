ee45737f19648b2eb38732f55c6c50382ca26206
Katie Nguyen <KT@Katies-MacBook.local>
2019-01-24 13:45:43 -0800

    updated to include gets() function

--- Classwork/Classwork01/SayHello.c
+++ Classwork/Classwork01/SayHello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-   int main( int argc, char * argv[] ) {
-       char   name[100];
-       printf( "Enter a name: " );
-       scanf("%s",name);
-
-       printf( "Hello, %s \n\n\n",name );
-   }
+int main( int argc, char * argv[] ) {
+    
+    char * name;
+    char input[25];
+    printf("Enter your name: " );
+    name = gets(input);
+    printf("\n\n   Hello, %s \n\n", name);
+    exit(0);
+    
+}
