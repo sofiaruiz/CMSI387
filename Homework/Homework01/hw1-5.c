34ad1e804b2502ec89248c3d730fd31452c0da4b
Moriah Tolliver <mtollive@lion.lmu.edu>
2019-02-05 16:49:08 -0800

    finalized problem 5

--- Homework/Homework01/hw1-5.c
+++ Homework/Homework01/hw1-5.c
@@ -2,21 +2,32 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static void *child(void *ignored){
-    sleep(3);
-    printf("Child is done sleeping 3 seconds.\n");
+static void *second(void *ignored){
+    while (1){
+        sleep(5);
+        printf("\nSecond thread slept 5 seconds.\n");
+    }
     return NULL;
 }
 
 int main(int argc, char *argv[]){
-    pthread_t child_thread;
+    pthread_t second_thread;
     int code;
 
-    code = pthread_create(&child_thread, NULL, child, NULL);
+    code = pthread_create(&second_thread, NULL, second, NULL);
     if(code){
         fprintf(stderr, "pthread_create failed with code %d\n", code);
     }
-    sleep(5);
-    printf("Parent is done sleeping 5 seconds.\n");
+
+    int key = 0;
+    printf("Press some keys: ");
+    while (key != 10) {
+        key = fgetc(stdin);
+    }
+    pthread_cancel(second_thread);
+    printf("Second thread has been canceled.\n");
     return 0;
 }
+
+// The sleeping thread can print its periodic messages while the main thread waits for keyboard input.
+// The main thread cannot read input, kill the sleeping thread, and print a message while the sleeping thread is in the early part of one of its five-second sleeps but it can appear to do so because of how fast the operating system switches threads.
