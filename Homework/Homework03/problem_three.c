5903549a1701260b09c38fbc2f05ef39cc7c083a
mtollive <mtollive@lion.lmu.edu>
2019-04-05 14:34:50 -0700

    change problem three to c

---
+++ Homework/Homework03/problem_three.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int main() {
+	pid_t returnedValue = fork();
+	if(returnedValue < 0) {
+		perror("error forking");
+		return -1;
+	} else if (returnedValue == 0){
+		if(open("/etc/passwd", O_RDONLY) < 0){
+			perror("error opening file");
+			return -1;
+		}
+		
+		execlp("tr", "tr", "a-z", "A-Z", NULL);
+		perror("error executing tr");
+		return -1;
+	} else {
+		if(waitpid(returnedValue, 0, 0) < 0) {
+			perror("error waiting for child");
+			return -1;
+		}
+	}
+}
+
+	
