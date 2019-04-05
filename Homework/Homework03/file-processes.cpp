200930470d543298454f3c8350b42a4625eb15c4
mtollive <mtollive@lion.lmu.edu>
2019-04-05 13:33:31 -0700

    Update file-processes.cpp

--- Homework/Homework03/file-processes.cpp
+++ Homework/Homework03/file-processes.cpp
@@ -5,33 +5,15 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-using namespace std;
 int main() {
-	pid_t returnedValue = fork();
-	if(returnedValue < 0) {
-		perror("error forking");
-		return -1;
-	} else if (returnedValue == 0){
-		if(close(STDOUT_FILENO) < 0){
-			perror("error closing standard output");
-			return -1;
-		}
-		
-		if(open("/etc/passwd", STDIN_FILENO) < 0){
-			perror("error opening /etc/passwd");
-			return -1;
-		}
-		execlp("tr", "tr", "a-z", "A-Z", NULL);
-		perror("error executing tr");
-		return -1;
-	} else {
-		if(waitpid(returnedValue, 0, 0) < 0) {
-			perror("error waiting for child");
-			return -1;
-		}
-		cout << "Note the parent still has the old standard output."
-		<< endl;
-	}
+if(open("/etc/passwd", STDIN_FILENO) < 0){
+	perror("error opening /etc/passwd");
+	return -1;
+    }
+
+    execlp("tr", "tr", "a-z", "A-Z", NULL);
+    perror("error executing tr");
+    return -1;
 }
 
 	
